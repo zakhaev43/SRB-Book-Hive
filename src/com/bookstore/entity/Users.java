55b515eff7d019582334b2dcd35cbe768bff64dd
imransuet <imransuet@gmail.com>
2020-07-16 16:50:06 +0200

    first commit

---
+++ src/com/bookstore/entity/Users.java
@@ -0,0 +1,56 @@
+package com.bookstore.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+public class Users {
+	private Integer userId;
+	private String email;
+	private String fullName;
+	private String password;
+
+	@Id
+	@Column(name = "user_id",unique=true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name = "full_name")
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ src/test/UsersTest.java
@@ -0,0 +1,30 @@
+package test;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.bookstore.entity.Users;
+
+public class UsersTest {
+
+	public static void main(String[] args) {
+		Users user1 = new Users();
+		user1.setEmail("imranset@gmail.com");
+		user1.setFullName("Sifur Rahman Bhuiyan");
+		user1.setPassword("hitan");
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(user1);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		System.out.println("A Users object was persisted");
+	}
+
+}
