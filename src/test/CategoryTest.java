cfafb69d65570bca96310b9bac4037e344ef57a8
imransuet <imransuet@gmail.com>
2020-07-17 08:43:09 +0200

    2

--- src/test/CategoryTest.java
+++ src/test/CategoryTest.java
@@ -3,22 +3,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.bookstore.entity.Category;
 import com.bookstore.entity.Users;
 
-public class UsersTest {
+public class CategoryTest {
 
 	public static void main(String[] args) {
-		Users user1 = new Users();
-		user1.setEmail("imrnset@gmail.com");
-		user1.setFullName("Sifur Rhman Bhuiyan");
-		user1.setPassword("hitan");
+		Category newCat = new Category("Advanced Java");
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
 		entityManager.getTransaction().begin();
 		
-		entityManager.persist(user1);
+		entityManager.persist(newCat);
 		
 		entityManager.getTransaction().commit();
 		entityManager.close();
